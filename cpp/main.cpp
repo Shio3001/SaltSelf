5729a5ea14346cf2f4aafdf471e15fca9e1784b0
Shio3001 <taku2000maru@icloud.com>
2021-09-28 15:57:43 +0900

    頂点と辺の追加

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -12,64 +12,6 @@ using namespace std;
 //namespace py = boost::python;
 //namespace np = boost::python::numpy;
 
-class SurfaceData
-{
-private:
-    std::string m_surface_data_key;
-    std::map<int, VertexXyzData *> m_edge_data;
-
-public:
-    SurfaceData(std::string send_surface_data_key)
-    {
-        m_surface_data_key = send_surface_data_key;
-        cout << "SurfaceData コンストラクタ" << endl;
-    }
-    ~SurfaceData()
-    {
-        cout << "SurfaceData デストラクタ" << endl;
-    }
-    void AddVertex(VertexXyzData vertex)
-    {
-        int vertex_size = GetVertexSize();
-        m_edge_data[vertex_size] = VertexXyzData;
-    }
-    void DeleteVertex(std::string vertex_key)
-    {
-        int vertex_size = GetVertexSize();
-        int discoveries = 0;
-
-        for (int i = 0; i < vertex_size; i++)
-        {
-            if (m_edge_data[vertex_size] == vertex_key)
-            {
-                m_edge_data.erase(vertex_size);
-                discoveries++;
-            }
-            else
-            {
-                m_edge_data[vertex_size - discoveries] = m_edge_data[vertex_size];
-            }
-        }
-    }
-
-    int GetVertexSize()
-    {
-        int vertex_size = m_edge_data.size();
-        return vertex_size;
-    }
-    std::vector<int> GetVertexKey()
-    {
-        std::vector<int> surface_data_key;
-        auto begin = m_edge_data.begin(), end = m_edge_data.end();
-        for (auto iter = begin; iter != end; iter++)
-        {
-            int key = iter->first;
-            surface_data_key.push_back(key);
-        }
-        return surface_data_key;
-    }
-};
-
 class VertexXyzData
 {
 private:
@@ -85,6 +27,11 @@ public:
     ~VertexXyzData()
     {
         cout << "VertexXyzData デストラクタ" << endl;
+    }
+
+    std::string VertexKey()
+    {
+        return m_vertex_xyz_data_key;
     }
 
     void Xyz(int x, int y, int z)
@@ -117,6 +64,66 @@ public:
     }
 };
 
+class SurfaceData
+{
+private:
+    std::string m_surface_data_key;
+    std::map<int, VertexXyzData *> m_edge_data;
+
+public:
+    SurfaceData(std::string send_surface_data_key)
+    {
+        m_surface_data_key = send_surface_data_key;
+        cout << "SurfaceData コンストラクタ" << endl;
+    }
+    ~SurfaceData()
+    {
+        cout << "SurfaceData デストラクタ" << endl;
+    }
+    void AddVertex(VertexXyzData &vertex)
+    {
+        int vertex_size = GetVertexSize();
+        m_edge_data[vertex_size] = &vertex;
+    }
+    void DeleteVertex(std::string vertex_key)
+    {
+        int vertex_size = GetVertexSize();
+        int discoveries = 0;
+
+        for (int i = 0; i < vertex_size; i++)
+        {
+            VertexXyzData *vertex_xyz_data = m_edge_data[vertex_size];
+
+            if (vertex_xyz_data->VertexKey() == vertex_key)
+            {
+                m_edge_data.erase(vertex_size);
+                discoveries++;
+            }
+            else
+            {
+                m_edge_data[vertex_size - discoveries] = m_edge_data[vertex_size];
+            }
+        }
+    }
+
+    int GetVertexSize()
+    {
+        int vertex_size = m_edge_data.size();
+        return vertex_size;
+    }
+    std::vector<int> GetVertexKey()
+    {
+        std::vector<int> surface_data_key;
+        auto begin = m_edge_data.begin(), end = m_edge_data.end();
+        for (auto iter = begin; iter != end; iter++)
+        {
+            int key = iter->first;
+            surface_data_key.push_back(key);
+        }
+        return surface_data_key;
+    }
+};
+
 class VertexControl
 {
 private:
@@ -133,6 +140,11 @@ public:
         AddVertexXyz("A", 0, 0, 0);
         AddVertexXyz("B", 100, 0, 0);
         AddVertexXyz("C", 0, 100, 0);
+
+        AddSurface("S");
+        AddVertexForSurface("S", "A");
+        AddVertexForSurface("S", "B");
+        AddVertexForSurface("S", "C");
     }
     ~VertexControl()
     {
@@ -148,7 +160,7 @@ public:
     {
         SurfaceData *surface_data = m_surface_data[surface_key];
         VertexXyzData *vertex_xyz_data = m_vertex_data[vertex_key];
-        surface_data->AddVertex(vertex_xyz_data);
+        surface_data->AddVertex(*vertex_xyz_data);
     }
 
     void AddVertexXyz(std::string key, int x, int y, int z)
@@ -191,13 +203,7 @@ int main()
         cout << test_get_xyz_key[i] << endl;
     }
 
-    vertex_control->DeleteVertexXyz(key); //テスト用コード
-
-    std::vector<std::string> test_get_xyz_key2 = vertex_control->GetVertexXyzDataKey();
-    for (int i = 0; i < test_get_xyz_key2.size(); i++) //テスト用コード
-    {
-        cout << test_get_xyz_key2[i] << endl;
-    }
+    //vertex_control->DeleteVertexXyz(key); //テスト用コード
 }
 
 // g++ -o main main.cpp -std=c++14
