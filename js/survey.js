e6f3b96aab55b96bf0994fc52a3ab2ae4ed0e1c9
Shio3001 <taku2000maru@icloud.com>
2021-11-02 09:52:38 +0900

    survey

---
+++ js/survey.js
@@ -0,0 +1,10 @@
+var survey_surface_key = document.getElementById('survey_surface_key');
+var survey_vertex_key = document.getElementById('survey_vertex_key');
+var survey_vertex_number = document.getElementById('survey_vertex_number');
+
+function consistency_survey() {
+    let surface_key = survey_surface_key.value
+    let vertex_key = survey_vertex_key.value
+    let vertex_number = parseFloat(survey_vertex_number.value)
+    vertex_control.ConsistencySurvey(surface_key, vertex_key, vertex_number)
+}
