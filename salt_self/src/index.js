2e72c12e6625c58bd6276ea05a486cee691e57f1
Shio3001 <taku2000maru@icloud.com>
2021-12-06 01:17:38 +0900

    react hello world

--- salt_self/src/index.js
+++ salt_self/src/index.js
@@ -1,36 +1,8 @@
-import React, { Component } from 'react';
-class my_self extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            todos: [],
-            name: ''
-        };
-    }
-    render() {
-        const { todos } = this.state;
-
-        return (<div>
-            <input type="text" />
-            <button>登録</button>
-            <ul>
-                {todos.map((todo, index) => <li key={index}>{todo}</li>)}
-            </ul>
-        </div>);
-    }
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
 
-const my_self_text1 = <p class="my_self_text">2004年12月14日生まれ</p>;
-const my_self_text2 = <p class="my_self_text" id="my_self_old">ここに年齢を挿入</p>;
-const my_self_text3 = <p class="my_self_text">2021年JOI 一次予選400点で通った</p>;
-const my_self_text4 = <p class="my_self_text">C++ Python</p>;
-ReactDOM.render(my_self_text1, document.getElementById('ps'));
-ReactDOM.render(my_self_text2, document.getElementById('ps'));
-ReactDOM.render(my_self_text3, document.getElementById('ps'));
-ReactDOM.render(my_self_text4, document.getElementById('ps'));
-
-var self_p_old = document.getElementById("my_self_old");
 const date_data = new Date();
 const year = date_data.getFullYear();
 const month = date_data.getMonth() + 1;
@@ -42,8 +14,23 @@ var age = year - 2004 - 1
 if (month > 12 && day > 14) {
     age++;
 }
-
 const text = "16進数で" + (age).toString(16) + "才"
 
-self_p_old.innerHTML = text;
+class MySelf extends React.Component {
+    render() {
+        return (
+            <div>
+                <p className="my_self_text">2004年12月14日生まれ</p>;
+                <p className="my_self_text">{text}</p>;
+                <p className="my_self_text">2021年JOI 一次予選400点で通った</p>;
+                <p className="my_self_text">C++ Python</p>;
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <MySelf />,
+    document.getElementById('root')
+);
 
