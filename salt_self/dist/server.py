d8aadfb1b4f00af39379940d695368075dc4cf81
Shio3001 <taku2000maru@icloud.com>
2022-01-20 16:05:39 +0900

    ビルドできた

--- salt_self/dist/server.py
+++ salt_self/dist/server.py
@@ -1,6 +1,8 @@
 import http.server
 import socketserver
 import sys
+import cgi
+
 Handler = http.server.SimpleHTTPRequestHandler
 
 port_number = int(input())
@@ -10,3 +12,5 @@ Handler.extensions_map['.wasm'] = 'application/wasm'
 with socketserver.TCPServer(("", port_number), Handler) as httpd:
     print("サーバ起動 port", port_number)
     httpd.serve_forever()
+
+# , "-cgi"
