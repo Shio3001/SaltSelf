7044a5981203f173049af85422abc6dc72ddafe8
Shio3001 <taku2000maru@icloud.com>
2022-01-26 02:02:20 +0900

    pub

--- salt_self/src/App.js
+++ salt_self/src/App.js
@@ -9,30 +9,30 @@ import * as Make3D_js from "./App/Salt3DCG/make3D.js";
 import "./App/Salt3DCG/make3D.css";
 
 // const myModule = await Module();
+let nowbasename;
+if (process.env.NODE_ENV == "production") {
+  nowbasename = "/SaltSelf";
+  console.log("production");
+} else {
+  nowbasename = "";
+  console.log("not production");
+}
+console.log("nowbasename", nowbasename);
+console.log("App render");
 
 class App extends React.Component {
   render() {
-    let nowbasename;
-    if (process.env.NODE_ENV == "production") {
-      nowbasename = "/SaltSelf";
-      console.log("production");
-    } else {
-      nowbasename = "";
-      console.log("not production");
-    }
-    console.log("nowbasename", nowbasename);
-    
-    console.log("App render");
+
     // console.log(process.env.PUBLIC_URL)
     // console.log(process.env.PUBLIC_PATH)
     return (
       // <Route exact path="/" element={<MySelf />} />
       // <Route exact path="/view" element={<Make3D_js.Make3D />} />
       <div className="App">
-        <BrowserRouter basename={nowbasename}>
+        <BrowserRouter>
           <Routes>
             <Route exact path="/" element={<MySelf />} />
-            <Route exact path="/view" element={<Make3D_js.Make3D />} />
+            <Route exact path="/SaltSelf/view" element={<Make3D_js.Make3D />} />
           </Routes>
         </BrowserRouter>
       </div>
