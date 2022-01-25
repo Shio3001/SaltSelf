b4c4f54a5e27779c48a748c79968eae7c9a0798c
Shio3001 <taku2000maru@icloud.com>
2022-01-26 03:02:21 +0900

    pub2

--- salt_self/src/App.js
+++ salt_self/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM, { render } from "react-dom";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
 
 import MySelf from "./App/MySelf/MySelf.js";
 import "./App/MySelf/MySelf.css";
@@ -30,11 +30,12 @@ class App extends React.Component {
 
       //r basename="https://shio3001.github.io/SaltSelf"
       <div className="App">
-
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route path="/" element={<MySelf />} />
             <Route path="/view" element={<Make3D_js.Make3D />} />
           </Routes>
+          </BrowserRouter>
       </div>
     );
   }
--- salt_self/src/index.js
+++ salt_self/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 
 //コンポーネント読み込み
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
 
 //(id="root"のところに)描画
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    
     <App />,
-    </BrowserRouter>,
+    
     document.getElementById('root')
 );
