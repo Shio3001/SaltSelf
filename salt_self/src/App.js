456cb7ab52eef1123f6c56f090c7cdfbde9f3397
Shio3001 <taku2000maru@icloud.com>
2022-01-26 16:37:13 +0900

    router

--- salt_self/src/App.js
+++ salt_self/src/App.js
@@ -22,6 +22,7 @@ console.log("App render");
 
 class App extends React.Component {
   render() {
+    console.log("BrowserRouter return");
     return (
       // <Route exact path="/" element={<MySelf />} />
       // <Route exact path="/view" element={<Make3D_js.Make3D />} />
