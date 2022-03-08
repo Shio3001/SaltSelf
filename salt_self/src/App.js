6d9ddf65f192a62d57fe7b2160926040096a1cfe
Shio3001 <taku2000maru@icloud.com>
2022-03-08 16:00:21 +0900

    cssアニメーションまわり

--- salt_self/src/App.js
+++ salt_self/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM, { render } from "react-dom";
-import { BrowserRouter,Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MySelf from "./App/MySelf/MySelf.js";
 import "./App/MySelf/MySelf.css";
@@ -23,27 +23,73 @@ if (process.env.NODE_ENV == "production") {
 console.log("nowbasename", nowbasename);
 console.log("App render");
 
+let header_flag = true //trueだと大きい状態、falseだと小さい状態
+
+function scroll(){
+  var scroll_y = window.scrollY;
+  console.log(scroll_y)
+
+
+  if (scroll_y > 100){ //小さくする
+    if (header_flag == true){
+      console.log("小さくする")
+      document.getElementById("header_element").style.animationName = "close_header";
+      header_flag = false
+    }
+  }
+  else{ //大きくする
+    if (header_flag == false){
+      console.log("大きくする")
+      document.getElementById("header_element").style.animationName = "open_header";
+      header_flag = true 
+    }
+  }
+  
+}
+
 class App extends React.Component {
+  componentDidMount(){
+    window.addEventListener("scroll", function(event) {
+      console.log("スクロールされました");
+      scroll()
+    });
+
+    scroll()
+  }
   render() {
+
+
     console.log("BrowserRouter return 9");
     return (
       // <Route exact path="/" element={<MySelf />} />
       // <Route exact path="/view" element={<Make3D_js.Make3D />} />
       <div className="App">
+        <header id="header_element">むじょはー</header>
 
-        <footer>むじょはー</footer>
-        {/* <MySelf /> */}
-        <BrowserRouter basename={nowbasename}>
-          <Routes>
-            <Route exact path="/"  element={<MySelf nowbasename={nowbasename} />} />
-            <Route path="/view" element={<Make3D_js.Make3D nowbasename={nowbasename}/>} />
-            <Route path="/entyping" element={<SaltEn_js.EnWordComponent nowbasename={nowbasename}/>} />
-            <Route path="*" element={<Test.TestComponent />} />
-          </Routes>
-        </BrowserRouter>
+        <div className="mainView">
+          <BrowserRouter basename={nowbasename}>
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={<MySelf nowbasename={nowbasename} />}
+              />
+              <Route
+                path="/view"
+                element={<Make3D_js.Make3D nowbasename={nowbasename} />}
+              />
+              <Route
+                path="/entyping"
+                element={
+                  <SaltEn_js.EnWordComponent nowbasename={nowbasename} />
+                }
+              />
+              <Route path="*" element={<Test.TestComponent />} />
+            </Routes>
+          </BrowserRouter>
+          <footer>なんか表示されないんだけど</footer>
+        </div>
 
-        <header>りんご</header>
-        
         {/* <Make3D_js.Make3D /> */}
       </div>
     );
