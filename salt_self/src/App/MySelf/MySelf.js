8729de6797711b127d5b0d667770e90d3b70317a
Shio3001 <taku2000maru@icloud.com>
2022-03-22 21:52:04 +0900

    font修正

--- salt_self/src/App/MySelf/MySelf.js
+++ salt_self/src/App/MySelf/MySelf.js
@@ -65,9 +65,9 @@ class MySelf extends React.Component {
 
         <p>趣味</p>
         <MySelfLink link_address={this.props.nowbasename + "/art"} link_text={"おえかき"} link_color="my_self_link_yellow" />
-{/*         
+        
         <p>界隈用</p>
-        <MySelfLink link_address={this.props.nowbasename + "/seat"} link_text={"座席記録"} link_color="my_self_link_gray" /> */}
+        <MySelfLink link_address={this.props.nowbasename + "/seat"} link_text={"座席記録"} link_color="my_self_link_gray" />
 
       </div>
     );
